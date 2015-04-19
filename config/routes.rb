c225d52bb95fc3763a0cb35440a38b723b745687
lezoudali <lezou.dali@gmail.com>
2015-04-18 20:43:13 -0400

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
   get 'static_pages/home'
 
   get 'static_pages/help'
+
+  get 'static_pages/about'
 
   root "application#hello"
 end
