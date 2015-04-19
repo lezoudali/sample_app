49654a1d54dd4ae4cefe06ac07d7522d859f3dce
lezoudali <lezou.dali@gmail.com>
2015-04-19 00:25:02 -0400

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  root "static_pages#home"
 
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
+  root             "static_pages#home"
+  get "help"    => "static_pages#help"
+  get "about"   => "static_pages#about"
+  get "contact" => "static_pages#contact"
+  get "signup"  => "users#new"
 
 end
